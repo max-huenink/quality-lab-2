4a870cd683e7d8aa160b4c0d7c1dd541edfb1ac9
Ian Seidler <seidler.ian@gmail.com>
2021-03-21 12:15:29 -0500

    -Added files and a unit tests for Mental Math game -Currently at red in TTD

---
+++ GainsProject/BigGainsTests/MentalMathGameTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GainsProject.Application;
+
+namespace BigGainsTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //---------------------------------------------------------------
+        //Tests the scoring in the mental math game manager
+        //---------------------------------------------------------------
+        [TestMethod]
+        public void scoringValid()
+        {
+            MentalMathGameManager game = new MentalMathGameManager();
+            game.setTime(5000);
+            game.calculateScore();
+            Assert.AreEqual(70, game.getScore());
+        }
+        //---------------------------------------------------------------
+        //Tests the scoring in the mental math game manager for wrong 
+        //answers
+        //---------------------------------------------------------------
+        [TestMethod]
+        public void scoring()
+        {
+            MentalMathGameManager game = new MentalMathGameManager();
+            game.setTime(-1);
+            game.calculateScore();
+            Assert.AreEqual(-50, game.getScore());
+        }
+        //---------------------------------------------------------------
+        //
+        //---------------------------------------------------------------
+        [TestMethod]
+        public void runGame()
+        {
+
+        }
+    }
+}
---
+++ GainsProject/GainsProject/Application/MentalMathGameManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using GainsProject.Domain;
+
+namespace GainsProject.Application
+{
+    public class MentalMathGameManager : BaseGame
+    {
+        public override void runGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void calculateScore()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int randomTime()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
