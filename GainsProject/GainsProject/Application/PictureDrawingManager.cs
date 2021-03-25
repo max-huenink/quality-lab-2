e6f0680ae9dca43afe122732b1d24a3b7eecee85
Ben Hefel <benben@mediacombb.net>
2021-03-24 19:50:23 -0500

    started implementing the base game class into the picture Drawing manager.

--- GainsProject/GainsProject/Application/PictureDrawingManager.cs
+++ GainsProject/GainsProject/Application/PictureDrawingManager.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿//---------------------------------------------------------------
+// Name:    Ben Hefel
+// Project: SE 3330 team:Xx_Bigger_Gains_xX
+// Purpose: To run the picture drawing game and manage the data
+//---------------------------------------------------------------
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GainsProject.Domain;
 
 namespace GainsProject.Application
 {
-    class PictureDrawingManager
+    class PictureDrawingManager : BaseGame
     {
+        //Seeded random
+        Random random = new Random();
+
+        public override void runGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void calculateScore()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int randomTime()
+        {
+            throw new NotImplementedException();
+            return -1;
+        }
     }
 }
