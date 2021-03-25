d8c6ed5e6ca857c4a3918dcb07385c81f0ee063f
Ben Hefel <benben@mediacombb.net>
2021-03-24 19:41:37 -0500

    added a userControl and a class

---
+++ GainsProject/GainsProject/UI/PictureDrawing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GainsProject.UI
+{
+    public partial class PictureDrawing : UserControl
+    {
+        public PictureDrawing()
+        {
+            InitializeComponent();
+        }
+    }
+}
