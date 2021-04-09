d54f91a10b38edfbefc8df80bb672cf21880f70d
Ian Seidler <seidler.ian@gmail.com>
2021-04-09 14:59:04 -0500

    Added name tests for TTD

---
+++ GainsProject/BigGainsTests/NameTests.cs
@@ -0,0 +1,37 @@
+﻿//---------------------------------------------------------------
+// Name:    Ian Seidler
+// Project: SE 3330 team:Xx_Bigger_Gains_xX
+// Purpose: To test the Name storage class
+//---------------------------------------------------------------
+using System;
+using GainsProject.Application;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BigGainsTests
+{
+    [TestClass]
+    public class NameTests
+    {    
+        //---------------------------------------------------------------
+        //Method to test setter and getter
+        //---------------------------------------------------------------
+        [TestMethod]
+        public void NameTest()
+        {
+            NameClass nameTest = new NameClass();
+            nameTest.setName("IAN");
+            Assert.AreEqual("IAN", nameTest.getName());
+        }
+        //---------------------------------------------------------------
+        //second Method to test setter and getter
+        //---------------------------------------------------------------
+        [TestMethod]
+        public void NameTest2()
+        {
+            NameClass nameTest = new NameClass();
+            nameTest.setName("IAN");
+            nameTest.setName("$$$");
+            Assert.AreEqual("$$$", nameTest.getName());
+        }
+    }
+}
--- GainsProject/GainsProject/Application/NameClass.cs
+++ GainsProject/GainsProject/Application/NameClass.cs
@@ -1,14 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿//---------------------------------------------------------------
+// Name:    Ian Seidler
+// Project: SE 3330 team:Xx_Bigger_Gains_xX
+// Purpose: To store the name and keep it seperate from the UI
+//---------------------------------------------------------------
 namespace GainsProject.Application
 {
     public class NameClass
     {
+        //Static name var to be shared among all objects
         static private string name;
+        //Getter and setter for name
         public void setName(string newName)
         {
             name = newName;
