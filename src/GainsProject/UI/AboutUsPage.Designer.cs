61edcc8f50d8c2a71c8bf91796cfbd5ffe537fb0
Ben Hefel <benben@mediacombb.net>
2021-05-06 22:06:41 -0500

    made about us page

--- src/GainsProject/UI/AboutUsPage.Designer.cs
+++ src/GainsProject/UI/AboutUsPage.Designer.cs
@@ -29,33 +29,34 @@ namespace GainsProject.UI
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutUsPage));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // pictureBox1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Rockwell", 24F);
-            this.label1.Location = new System.Drawing.Point(430, 48);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(222, 36);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Leaderboards";
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-46, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1109, 711);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
-            // LeaderboardPage
+            // AboutUsPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.label1);
-            this.Name = "LeaderboardPage";
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "AboutUsPage";
             this.Size = new System.Drawing.Size(1063, 681);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
